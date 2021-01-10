bd623fc68f43deb4473d6cbb5b01409c6670bf89
Yosef Gamble <yegamble@gmail.com>
2021-01-11 09:55:37 +1300

    modify migration

--- database/migrations/2021_01_10_204831_add_date_to_tasks.php
+++ database/migrations/2021_01_10_204831_add_date_to_tasks.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTaskTable extends Migration
+class AddDateToTasks extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,11 @@ class CreateTaskTable extends Migration
      */
     public function up()
     {
-        Schema::create('tasks', function (Blueprint $table) {
+        Schema::table('tasks', function (Blueprint $table) {
             //
-            $table->id();
-            $table->unsignedBigInteger('user_id');
-            $table->text('description');
+            $table->dateTime('updated_at');
+            $table->dateTime('created_at');
+            $table->text("description");
         });
     }
 
