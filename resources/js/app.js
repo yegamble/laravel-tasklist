790051d05606dafbde39702d871f79d31879501e
Yosef Gamble <yegamble@gmail.com>
2021-01-11 09:30:20 +1300

    add vuetify and task controller.

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,11 @@ require('./bootstrap');
 
 window.Vue = require('vue').default;
 
+
+import Vuetify from 'vuetify';
+Vue.use(Vuetify);
+
+
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
@@ -29,4 +34,5 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 
 const app = new Vue({
     el: '#app',
+    vuetify: new Vuetify(),
 });
