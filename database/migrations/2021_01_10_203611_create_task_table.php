5f381ae569ce4241c4cf8afa9c1ccfdaee5f6fee
Yosef Gamble <yegamble@gmail.com>
2021-01-11 09:45:22 +1300

    add task relationship.

---
+++ database/migrations/2021_01_10_203611_create_task_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTaskTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            //
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->text('task');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            //
+        });
+    }
+}
